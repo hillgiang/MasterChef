8aa4de282601049af881bc4abb9860f843b532c6
Hillary <hillgiang@gmail.com>
2017-08-09 20:11:19 +0800

    Begin Page
    Logo show up

--- app/src/main/java/com/project/cyim/masterchef/UserMore.java
+++ app/src/main/java/com/project/cyim/masterchef/UserMore.java
@@ -1,7 +1,5 @@
 package com.project.cyim.masterchef;
 
-import android.app.ActionBar;
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
@@ -9,36 +7,56 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 /**
  * Created by Hillary on 8/6/2017.
  */
 
 public class UserMore extends AppCompatActivity {
-    String[] moreArray = {"Log Out"};
+    String[] moreArray = new String[3];
+    private ListView listView;
+    SessionManagement session;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_more);
+        moreArray[0] = getString(R.string.language_text);
+        moreArray[1] = getString(R.string.aboutus_text);
+        moreArray[2] = getString(R.string.logout_text);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        session = new SessionManagement(getApplicationContext());
 
         ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.usermore_list, moreArray);
-        ListView listView = (ListView) findViewById(R.id.list);
+        listView = (ListView) findViewById(R.id.list);
         listView.setAdapter(adapter);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String item = ((TextView)view).getText().toString();
+                if (item.equals(getString(R.string.language_text))) {
 
+                } else if (item.equals(getString(R.string.aboutus_text))) {
+
+                } else if (item.equals(getString(R.string.logout_text)))
+                    session.logoutUser();
             }
         });
     }
 
-    public boolean onOptionsItemSelected(MenuItem item){
-        Intent myIntent = new Intent(getApplicationContext(), UsersInfo.class);
-        startActivityForResult(myIntent, 0);
-        return true;
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // this takes the user 'back', as if they pressed the left-facing triangle icon on the main android toolbar.
+                // if this doesn't work as desired, another possibility is to call `finish()` here.
+                super.onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
