1903b4c1143c67a64229d2f51dd3da926b52ea3b
Hillary <hillgiang@gmail.com>
2017-10-20 12:16:58 +0800

    Upload

---
+++ app/src/main/java/com/project/cyim/masterchef/UploadReci.java
@@ -0,0 +1,27 @@
+package com.project.cyim.masterchef;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by yu fen on 2017/10/14.
+ */
+
+public class UploadReci extends Fragment{
+    public UploadReci() {
+        // Required empty public constructor
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.upload_reci, container, false);
+    }
+}
